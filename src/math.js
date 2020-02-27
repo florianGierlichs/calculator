6a13e4787cb2e317b6330365050ecc51ba3f66d5
florianGierlichs <gierlichs@gmx.de>
2020-02-27 11:06:27 +0100

    Add math.js

---
+++ src/math.js
@@ -0,0 +1,16 @@
+// calculations
+export const addition = (numberOne, numberTwo) => {
+  return numberOne + numberTwo;
+};
+
+export const subtract = (numberOne, numberTwo) => {
+  return numberOne - numberTwo;
+};
+
+export const divide = (numberOne, numberTwo) => {
+  return numberOne / numberTwo;
+};
+
+export const multiply = (numberOne, numberTwo) => {
+  return numberOne * numberTwo;
+};
