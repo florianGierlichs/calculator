9e7eedb28dbd5b81a303853e05ec9fad9f89730f
Florian Gierlichs <58342773+florianGierlichs@users.noreply.github.com>
2020-03-01 16:51:34 +0100

    Slide in (#15)
    * Add the magic
    
    * Add the magic button
    
    * Build first slide in effect
    
    * You wont belive your eyes
    
    * Update calculator.js
    
    * Update calculator.js
    
    * Update calculator.js
    
    * Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -14,11 +14,36 @@ let numberOne = "";
 let numberTwo = "";
 let operator = "";
 
+const randomizeShit = () => {
+  //do work
+  const listInputs = document.querySelectorAll(".calculator__inputs");
+  const listSpec = document.querySelectorAll(".calculator__input__spec");
+  const listTotal = [...listInputs, ...listSpec]; // new Array()
+
+
+  listTotal.forEach(item => {
+    const randNum = Math.floor(Math.random() * (1000 - 700 + 1) + 700);
+
+    const moreRandom = Math.random();
+
+    if (moreRandom > 0.5) {
+      item.style.left = `${randNum}px`;
+    } else {
+      item.style.left = `-${randNum}px`;
+    }
+
+    item.style.top = `-${randNum}px`;
+  });
+};
+
+document.addEventListener("DOMContentLoaded", randomizeShit);
+
 // functions
 function erase() {
   calculatorOutput.value = "";
 }
 calculatorClear.addEventListener("click", erase);
+
 //
 calculatorInputs.forEach(callInput => {
   callInput.addEventListener("click", () => {
@@ -51,8 +76,34 @@ calculatorResult.addEventListener("click", handleResultClick);
 
 const magic = document.querySelector(".magicBtn");
 
+
+//
+
+const inputsMagic = className => {
+  const allInputs = document.querySelectorAll(className);
+
+  const handleActive = item => {
+    return item.classList.toggle("active");
+  };
+
+  return allInputs.forEach(handleActive);
+};
+
 const doTheMagic = () => {
-  document.querySelector(".calculator__wrapper").classList.toggle("active");
+  document.querySelector(".calculator__wrapper").classList.toggle("calcActive");
+  document.querySelector(".clear").classList.toggle("active");
+  document.querySelector(".calculator").classList.toggle("calcActive");
+  document
+    .querySelector(".calculator__input__result")
+    .classList.toggle("active");
+
+  // Select all inputs and add active
+  inputsMagic(".calculator__inputs");
+  // select all input specs and add active
+  inputsMagic(".calculator__input__spec");
+
+  randomizeShit();
+  return;
 };
 
 magic.addEventListener("click", doTheMagic);
